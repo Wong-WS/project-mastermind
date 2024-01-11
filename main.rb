7901c73b83aa42b1227aa95555a898fb4d2e8f5e
Wong <66583478+Wong-WS@users.noreply.github.com>
2024-01-11 11:31:01 +0800

    Add computer solve logic, project is done

--- main.rb
+++ main.rb
@@ -35,8 +35,30 @@ class PlayerMaker
   end
 
   def first_guess
-    @comp_input_code = Array.new(4) { rand(1..6) }
-    puts "#{@comp_input_code}"
+    @comp_input_code = generate_random_code
+    puts "Computer guessed: #{@comp_input_code}"
+  end
+
+  def solve
+    new_guess = generate_new_guess(@player_input_code, find_matching_indices(@player_input_code, @comp_input_code))
+    @comp_input_code = new_guess
+    puts "Computer guessed: #{@comp_input_code}"
+  end
+
+  private
+
+  def generate_random_code
+    Array.new(4) { rand(1..6) }
+  end
+
+  def find_matching_indices(code1, code2)
+    code1.each_index.select { |i| code1[i] == code2[i] }
+  end
+
+  def generate_new_guess(previous_guess, matches)
+    new_guess = previous_guess.map.with_index do |digit, index|
+      matches.include?(index) ? digit : rand(1..6)
+    end
   end
 end
 
@@ -79,6 +101,7 @@ class Board
       check_win(@player_breaker.player_input_code)
       check_code(@player_breaker.player_input_code, @secret_code)
       @turn_count += 1
+      puts "\r\n"
     end
     result
   end
@@ -86,6 +109,17 @@ class Board
   def play_player_maker
     @player_maker.player_input
     @player_maker.first_guess
+    check_code(@player_maker.player_input_code, @player_maker.comp_input_code)
+    @turn_count += 1
+    until @turn_count >= 13
+      puts "Turn: #{@turn_count}"
+      @player_maker.solve
+      check_win_comp
+      check_code(@player_maker.player_input_code, @player_maker.comp_input_code)
+      @turn_count += 1
+      puts "\r\n"
+    end
+    result
   end
 
 
@@ -114,6 +148,27 @@ class Board
     end
   end
 
+  def check_code_comp(player_input_code, comp_input_code)
+
+    matching_digits = player_input_code.zip(comp_input_code).select { |a, b| a == b}.map(&:first)
+
+    if matching_digits.empty?
+      puts "Match: 0"
+    else
+      puts "Match: #{matching_digits.length}"
+    end
+
+    partial_digits = player_input_code.select { |number| secret_code.include?(number) }
+
+    partial_digits -= matching_digits
+
+    if partial_digits.empty?
+      puts "Partial: 0"
+    else
+      puts "Partial: #{partial_digits.length}"
+    end
+  end
+
   def check_win(player_input_code)
     if player_input_code == @secret_code
       @turn_count = 13
@@ -121,11 +176,27 @@ class Board
     end
   end
 
+  def check_win_comp
+    if @player_maker.comp_input_code == @player_maker.player_input_code
+      @turn_count = 13
+      @winner = true
+    end
+  end
+
   def result
-    if @winner == true
-      puts "Congratulations, you solved it!"
+    case @player_choice
+    when "1"
+      if @winner == true
+        puts "Congratulations, you solved it!"
+      else
+        puts "The code was #{@secret_code}. Better luck next time!"
+      end
     else
-      puts "The code was #{@secret_code}. Better luck next time!"
+      if @winner == true
+        puts "The machine figured out your code!"
+      else
+        puts "You beat the machine!"
+      end
     end
   end
 end
